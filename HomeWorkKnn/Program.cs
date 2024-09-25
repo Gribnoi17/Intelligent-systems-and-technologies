ba0c342e5d66e27177ed2f6d63d57b16e2a44f70
Gribanov Danil <danil.gribanov2017@gmail.com>
2024-09-25 21:40:56 +0300

    Добавил exe файл для запуска

--- HomeWorkKnn/Program.cs
+++ HomeWorkKnn/Program.cs
@@ -2,7 +2,9 @@
 using HomeWorkKnn.MachineLearning.Helpers;
 using HomeWorkKnn.MachineLearning.KNN;
 
-var knn = new Knn(25);
+Console.Write("Введите значение K: ");
+var k = int.Parse(Console.ReadLine());
+var knn = new Knn(k);
 
 var data = CsvReader.LoadDataFromCsv("data.csv");
 MachineLearningHelper.SplitData(data, out var trainData, out var testData, 0.8);
@@ -12,4 +14,5 @@ var accuracy = knn.CalculateAccuracy(testData);
 Console.WriteLine($"Accuracy: {accuracy * 100}%");
 
 knn.SaveModel("model.json");
+Console.ReadLine();
 
